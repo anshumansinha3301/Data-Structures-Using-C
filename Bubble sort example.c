436a721093736d9734fad03c0d645822902eff05
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-26 00:41:45 +0530

    Add files via upload

---
+++ Bubble sort example.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+void bubbleSort(int arr[], int n);
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    int i;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int arr[], int n) {
+    int i = 0, j = 0;
+    while (i < n - 1) {
+        j = 0;
+        while (j < n - i - 1) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+            j++;
+        }
+        i++;
+    }
+}

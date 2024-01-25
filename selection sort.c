aad4c91f81e6c160599443a0058cc550f478a259
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-26 00:44:46 +0530

    Add files via upload

---
+++ selection sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+void selectionSort(int arr[], int n);
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    int i;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    selectionSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex;
+
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(&arr[minIndex], &arr[i]);
+    }
+}

b972a60379f10693fee7afb16b965329768ddd99
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-31 10:58:45 +0530

    selection sort.c

--- selection sort.c
+++ selection sort.c
@@ -13,14 +13,12 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
     selectionSort(arr, n);
 
     printf("Sorted array:\n");
     for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-
     return 0;
 }
 

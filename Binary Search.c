983c748f5a9fb6d94753fad570eb3fb3f896ec77
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-01 22:18:42 +0530

    Binary Search.c

--- Binary Search.c
+++ Binary Search.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-
 int binarySearch(int arr[], int low, int high, int target) {
     while (low <= high) {
         int mid = low + (high - low) / 2;
 
         if (arr[mid] == target)
             return mid;
-
         if (arr[mid] < target)
             low = mid + 1;
         else
             high = mid - 1;
     }
-
     return -1;
 }
-
 int main() {
     int n, target;
     printf("Enter the size of the array: ");

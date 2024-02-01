94de0a8f2260367fb25d71bdd63b19b6facd0d29
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-01 16:20:14 +0530

    Linear Search.c

---
+++ Linear Search.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n, target;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &target);
+
+    int index = linearSearch(arr, n, target);
+
+    if (index != -1) {
+        printf("Element %d found at index %d\n", target, index);
+    } else {
+        printf("Element %d not found in the array\n", target);
+    }
+
+    return 0;
+}

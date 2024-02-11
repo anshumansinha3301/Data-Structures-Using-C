123f8761bf90ca8dd649b127905de4e601e51ebf
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-11 10:35:27 +0530

    Add files via upload

---
+++ quicksort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; 
+    int i = (low - 1);    
+
+    int j;
+    for (j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+
+        quicksort(arr, low, pivot - 1);
+        quicksort(arr, pivot + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: \n");
+    printArray(arr, n);
+
+    quicksort(arr, 0, n - 1);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}

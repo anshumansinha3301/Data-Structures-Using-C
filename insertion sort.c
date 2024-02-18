77278ba34f24b8c625fd0df5b165234ddd5d0475
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-18 08:52:09 +0530

    insertion sort.c

--- insertion sort.c
+++ insertion sort.c
@@ -4,7 +4,7 @@ void insertionSort(int arr[], int n) {
     for (i = 1; i < n; i++) {
         key = arr[i];
         j = i - 1;
-
+ 
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;

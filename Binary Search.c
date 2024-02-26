e9fd100f74a66439e80202ab52f19938d7ad2102
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-26 14:52:45 +0530

    Binary Search.c

--- Binary Search.c
+++ Binary Search.c
@@ -11,6 +11,7 @@ int binarySearch(int arr[], int low, int high, int target) {
             high = mid - 1;
     }
     return -1;}
+
 int main() {
     int n, target;
     printf("Enter the size of the array: ");
@@ -21,6 +22,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    
     printf("Enter the element to search: ");
     scanf("%d", &target);
     int index = binarySearch(arr, 0, n - 1, target);
